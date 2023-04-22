119b7bd7bf297884d30df59981d2756abcdc2950
oksd0340 <oksd0340@gmail.com>
2023-04-23 05:50:51 +0900

    feat : 투두 리스트 페이지 추가
    - 투두 리스트 컨트롤러 추가
    - 투두 리스트 html 파일 추가
    - 투두 리스트 컨트롤러 작동 테스트 완료

---
+++ web/src/test/java/org/dizzyfox734/todo_list/web/TodoControllerTest.java
@@ -0,0 +1,28 @@
+package org.dizzyfox734.todo_list.web;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.assertj.core.api.Java6Assertions.assertThat;
+import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.RANDOM_PORT;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment = RANDOM_PORT)
+public class TodoControllerTest {
+
+    @Autowired
+    private TestRestTemplate restTemplate;
+
+    @Test
+    public void load_todoPage() {
+        // when
+        String body = this.restTemplate.getForObject("/todo", String.class);
+
+        // then
+        assertThat(body).contains("Todo list");
+    }
+}
