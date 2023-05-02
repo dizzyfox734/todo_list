8bb750197ad1634a75e6ec6ad67d29a9bb4cb908
oksd0340 <oksd0340@gmail.com>
2023-05-02 23:12:40 +0900

    feat : todo 수정 기능 추가
    - 수정 입력 모달 추가
    - 모달 css 추가
    - api와 연결하는 js 함수 추가

--- todo_list/src/main/resources/static/js/todo.js
+++ todo_list/src/main/resources/static/js/todo.js
@@ -1,3 +1,5 @@
+const modal = document.getElementById("modal");
+
 function saveTodo() {
     let data = {
         content: document.getElementById('todo-save').value,
@@ -23,6 +25,52 @@ function deleteTodo(id) {
         method: "DELETE",
     }).then((response) => response.json()
     ).then((data) => {
+        alert("할 일이 삭제되었습니다.");
         location.reload();
     }).catch((error) => console.log(error));
+}
+
+function completeTodo(id) {
+    const url = "/api/todo/complete/"+id;
+
+    fetch(url, {
+        method: "POST",
+    }).then((response) => response.json()
+    ).then((data) => {
+        alert("할 일이 완료되었습니다.");
+        location.reload();
+    }).catch((error) => console.log(error));
+}
+
+function updateTodo() {
+    const id = document.getElementById('todo-id').value;
+    const url = "/api/todo/"+id;
+
+    let data = {
+        content: document.getElementById('todo-update').value,
+        completed_fl: false,
+    };
+
+    fetch(url, {
+        method: "PUT",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify(data),
+    }).then((response) => response.json()
+    ).then((data) => {
+        alert("수정 되었습니다.");
+        location.reload();
+    }).catch((error) => console.log(error));
+}
+
+function openModal(id) {
+    document.getElementById('todo-id').value = id;
+
+    modal.classList.remove("hidden");
+}
+
+function closeModal() {
+    document.getElementById('todo-id').value = '';
+    modal.classList.add("hidden");
 }
